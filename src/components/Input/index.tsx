3186a729999fe022932ade038291b1365e49da8b
Matheus Melo <matheus.correia@lavid.ufpb.br>
2023-03-04 14:19:11 -0300

    input refactored

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,11 +6,18 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
+import { IconType} from 'react-icons';
+
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon: IconType;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
